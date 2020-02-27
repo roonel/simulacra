e43b7bd57196e0cb59d0989f14c66d67c9d1849f
Mark Fodor <akosmark.fodor@gmail.com>
2020-02-27 16:09:42 +0900

    Deploy fixes

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('demonlord-app app is running!');
+    expect(page.getTitleText()).toEqual('shadow-tools app is running!');
   });
 
   afterEach(async () => {
