fce01a320dfb183d39547dd6546c87cdb314efd1
Mark Fodor <akosmark.fodor@gmail.com>
2020-02-29 04:01:19 +0900

    Use markdown for processed text

--- src/app/shared/processed-text/processed-text.component.ts
+++ src/app/shared/processed-text/processed-text.component.ts
@@ -1,5 +1,6 @@
 import {Component, Input, OnInit} from '@angular/core';
 import {Router} from '@angular/router';
+import {Remarkable} from 'remarkable';
 
 @Component({
   selector: 'app-processed-text',
@@ -9,6 +10,8 @@ import {Router} from '@angular/router';
 export class ProcessedTextComponent implements OnInit {
   // tslint:disable-next-line:variable-name
   private _text: string;
+  private md: Remarkable = new Remarkable();
+
   get text(): string {
     return this._text;
   }
@@ -26,68 +29,102 @@ export class ProcessedTextComponent implements OnInit {
   }
 
   ngOnInit(): void {
+
   }
 
   processText() {
-    let isActionBlock = false;
-    let openings = 0;
-    let currentText = '';
-    const list = [];
-    for (let i = 0; i < this._text.length; i++) {
-      const element = this._text[i];
-      if (!isActionBlock) {
-        if (element === '<' && this._text[i + 1] === '<') {
-          isActionBlock = true;
-          i++;
-          list.push({type: 'text', value: currentText});
-          currentText = '';
-        } else {
-          currentText += element;
-        }
-      } else {
-        if (element === '<' && this._text[i + 1] === '<') {
-          openings++;
-          currentText += element;
-          currentText += element;
-          i++;
-        } else if (element === '>' && this._text[i + 1] === '>') {
-          if (openings > 0) {
-            openings--;
-            currentText += element;
-            currentText += element;
+    let elements = [];
+    const blockLevel = [];
+    this.md.block.parse(this._text, {}, {}, blockLevel);
+    for (let i = 0; i < blockLevel.length; i++) {
+      const current = blockLevel[i];
+      if (current.type === 'paragraph_open') {
+        const paragraph = this.getParagraph(blockLevel, i);
+        elements = elements.concat(paragraph);
+        i += 2;
+      } else if (current.type === 'bullet_list_open') {
+        i++;
+        let next = blockLevel[i];
+        const listItems = [];
+        while (next.type !== 'bullet_list_close') {
+          if (next.type === 'list_item_open') {
+            let currentListItem = [];
             i++;
-          } else {
-            isActionBlock = false;
-            i++;
-            list.push(this.determineAction(currentText));
-            currentText = '';
+            next = blockLevel[i];
+            while (next.type !== 'list_item_close') {
+              const paragraph = this.getParagraph(blockLevel, i);
+              currentListItem = currentListItem.concat(paragraph);
+              i += 3;
+              next = blockLevel[i];
+            }
+            listItems.push(currentListItem);
           }
-        } else {
-          currentText += element;
+          i++;
+          next = blockLevel[i];
         }
+        elements.push({type: 'list', value: listItems});
+      } else if (current.type === 'heading_open') {
+        i++;
+        const headingInline = [];
+        const next = blockLevel[i];
+        this.md.inline.parse(next.content, {}, {}, headingInline);
+        elements.push({type: 'header', value: headingInline[0].content, level: current.hLevel});
+        i++;
       }
     }
-    if (currentText.length > 0) {
-      list.push({type: 'text', value: currentText});
-    }
-    this.dataList = list;
+    this.dataList = elements;
   }
 
-  determineAction(actionText: string) {
-    if (actionText === 'br') {
-      return {type: 'break'};
-    }
-    const actionType = actionText.substring(0, actionText.indexOf(':'));
-    const actionData = actionText.substring(actionText.indexOf(':') + 1);
-    if (actionType === 'table' || actionType === 'list') {
-      const validJson = actionData.replace('/\'', '’').replace(/\'/g, '"');
-      return {type: actionType, value: JSON.parse(validJson)};
-    } else {
-      return {type: actionType, value: actionData};
-    }
+  navigate(link: string) {
+    const linkParts = link.split('/');
+    this.router.navigate([linkParts[0] + '-list', linkParts[1]]);
   }
 
-  navigate(item: any) {
-    this.router.navigate([item.type + '-list', item.value]);
+  private getParagraph(blockLevel: any[], i: number) {
+    const current = blockLevel[i];
+
+    const inlineData = [];
+    if (current.type === 'paragraph_open') {
+      let next = blockLevel[i + 1];
+      if (next.type !== 'inline') {
+        console.warn('error');
+      }
+      this.md.inline.parse(next.content, {}, {}, inlineData);
+      next = blockLevel[i + 2];
+
+      if (next.type !== 'paragraph_close') {
+        console.warn('error');
+      }
+    }
+    return this.processInline(inlineData);
+  }
+
+  private processInline(inlineData: any[]) {
+    const dataList = [];
+    for (let i = 0; i < inlineData.length; i++) {
+      const current = inlineData[i];
+      if (current.type === 'softbreak') {
+        dataList.push({type: 'break'});
+      } else if (current.type === 'strong_open') {
+        i++;
+        const text = inlineData[i];
+        dataList.push({type: 'bold', value: text.content});
+        i++;
+      } else if (current.type === 'em_open') {
+        i++;
+        const text = inlineData[i];
+        dataList.push({type: 'italics', value: text.content});
+        i++;
+      } else if (current.type === 'link_open') {
+        const href = current.href;
+        i++;
+        const text = inlineData[i];
+        dataList.push({type: 'link', value: text.content, link: href});
+        i++;
+      } else if (current.type === 'text') {
+        dataList.push({type: 'text', value: current.content});
+      }
+    }
+    return dataList;
   }
 }
