a4edb8f085e920656d3dbd1bf9a587790f899855
Mark Fodor <akosmark.fodor@gmail.com>
2020-02-28 16:34:12 +0900

    Casting pipe fix

--- src/app/shared/castings.pipe.ts
+++ src/app/shared/castings.pipe.ts
@@ -1,19 +1,34 @@
-import { Pipe, PipeTransform } from '@angular/core';
+import {Pipe, PipeTransform} from '@angular/core';
 import {ContentService} from '../content.service';
 
 @Pipe({
   name: 'castings'
 })
 export class CastingsPipe implements PipeTransform {
+
+  private castingByRank: [
+    [1],
+    [2, 1],
+    [3, 2, 1],
+    [4, 2, 1, 1],
+    [5, 2, 2, 1, 1],
+    [6, 3, 2, 2, 1, 1],
+    [7, 3, 2, 2, 2, 1, 1],
+    [8, 3, 2, 2, 2, 1, 1, 1],
+    [9, 3, 3, 2, 2, 2, 1, 1, 1],
+    [10, 3, 3, 3, 2, 2, 1, 1, 1, 1],
+    [11, 3, 3, 3, 3, 2, 1, 1, 1, 1, 1]
+  ];
+
   constructor(private contentService: ContentService) {
   }
 
   transform(spellId: string, power: number): string {
     const spell = this.contentService.getSpellList().find(s => s.name === spellId);
     if (spell) {
-      return '<<spell:' + spellId + '>> (' + (power + 1 - spell.level) + ')' ;
+      return '<<spell:' + spellId + '>> (' + this.castingByRank[power][spell.level] + ')';
     } else {
-      return '<<spell:' + spellId + '>> (?)' ;
+      return '<<spell:' + spellId + '>> (?)';
     }
   }
 
