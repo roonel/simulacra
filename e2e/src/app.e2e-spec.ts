4749e57beac814a9b58a8f90d7d3ba0c3048001f
Mark Fodor <akosmark.fodor@gmail.com>
2020-03-05 18:45:38 +0900

    Renaming the app

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('shadow-tools app is running!');
+    expect(page.getTitleText()).toEqual('simulacra app is running!');
   });
 
   afterEach(async () => {
--- server.js
+++ server.js
@@ -3,11 +3,11 @@ const path = require('path');
 const app = express();
 
 // Serve static files....
-app.use(express.static(__dirname + '/dist/shadow-tools'));
+app.use(express.static(__dirname + '/dist/simulacra'));
 
 // Send all requests to index.html
 app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + '/dist/shadow-tools/index.html'));
+  res.sendFile(path.join(__dirname + '/dist/simulacra/index.html'));
 });
 
 // default Heroku PORT
