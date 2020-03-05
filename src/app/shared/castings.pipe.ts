e9a42430b7188d514485c5d17acd0bb261b7d7b1
Mark Fodor <akosmark.fodor@gmail.com>
2020-03-05 15:16:34 +0900

    Bugfixing

--- src/app/shared/castings.pipe.ts
+++ src/app/shared/castings.pipe.ts
@@ -24,11 +24,11 @@ export class CastingsPipe implements PipeTransform {
   }
 
   transform(spellId: string, power: number): string {
-    const spell = this.contentService.getSpellList().find(s => s.name === spellId);
+    const spell = this.contentService.getSpellList().find(s => s.id === spellId);
     if (spell) {
-      return '[' + spell.name + '](spell/' + spellId.replace(/\s/g, '') + ') (' + this.castingByRank[power][spell.level] + ')';
+      return '[' + spell.id + '](spell/' + spellId.replace(/\s/g, '') + ') (' + this.castingByRank[power][spell.level] + ')';
     } else {
-      return '[' + spell.name + '](spell/' + spellId + ') (?)';
+      return '[' + spell.id + '](spell/' + spellId + ') (?)';
     }
   }
 
