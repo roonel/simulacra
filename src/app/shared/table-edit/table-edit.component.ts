74d473849a0301fb89f841103d0562ff7986e7a0
Mark Fodor <akosmark.fodor@gmail.com>
2020-03-13 19:36:02 +0900

    Better table edit

--- src/app/shared/table-edit/table-edit.component.ts
+++ src/app/shared/table-edit/table-edit.component.ts
@@ -14,4 +14,30 @@ export class TableEditComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  addCol() {
+    if (!this.table.columns) {
+      this.table.columns = [];
+    }
+    this.table.columns.push('');
+  }
+
+  removeRow(i: number) {
+    this.table.rows.splice(i, 1);
+  }
+
+  addRow() {
+    if (!this.table.rows) {
+      this.table.rows = [];
+    }
+    const empty = [];
+    // tslint:disable-next-line:prefer-for-of
+    for (let i = 0; i < this.table.columns.length; i++ ) {
+      empty.push('');
+    }
+    this.table.rows.push(empty);
+  }
+
+  trackByFn(index: any, item: any) {
+    return index;
+  }
 }
